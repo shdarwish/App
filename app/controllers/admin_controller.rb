7417d3f4c5ce6f959ca666df0710fd2133b97528
Shaikha Al-Darwish <sdarwish@qatar.cmu.edu>
2016-03-26 15:42:19 +0300

    edit

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,15 @@
 class AdminController < ApplicationController
 
-	before_action :admin_only
+#	before_action :admin_only
 
 	def index
 	end
 
-	private
+#	private
 
-	def admin_only
-		if !current_user.admin?
-			redirect_to root_path
-		end
-	end
+#	def admin_only
+#		if !current_user.admin?
+#			redirect_to root_path
+#		end
+#	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :admin_only
+#  before_action :admin_only
 
   # GET /users
   # GET /users.json
@@ -76,11 +76,11 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :password, :password_confirmation, :admin)
     end
 
-    def admin_only
-      if !current_user.admin?
-        redirect_to root_path
-      end 
-    end
+ #   def admin_only
+  #    if !current_user.admin?
+   #     redirect_to root_path
+   #   end 
+   # end
 
 
 end
